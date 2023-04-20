c87c539613b70f035a1317d184357135c9718585
Joffre Hermosilla Salas <joffre.hermosilla@gmail.com>
2023-04-19 19:45:44 -0500

    chore[1] actualizacion de bootstrap MS singularidad configuraciones y conexion open ai

---
+++ src/test/java/hackathon/nttdata/coderpath/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package hackathon.nttdata.coderpath.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
